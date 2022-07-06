243f11d92edae9aee1d1054ef6b85b6335d2a064
Fernweh99 <daniele.dimento@gmail.com>
2022-07-06 14:56:20 +0200

    finish milestone 1

--- js/script.js
+++ js/script.js
@@ -12,3 +12,45 @@ Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 *Non è necessario creare date casuali, inventatele*
 *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=3)
 */
+
+let i = 1;
+
+const post = [
+  {
+    id: i++,
+    autorName: 'Negan Smith',
+    autorImage: 'https://unsplash.it/300/300?image=3',
+    date: '05/25/2015',
+    postText: 'lorem ipsum dolor sit amet, consectet',
+    postImage: 'https://unsplash.it/300/300?image=8',
+    numberLikes: 105,
+  },
+  {
+    id: i++,
+    autorName: 'Peter Parker',
+    autorImage: 'https://unsplash.it/300/300?image=4',
+    date: '10/21/2018',
+    postText: 'lorem ipsum dolor sit amet, consectet',
+    postImage: 'https://unsplash.it/300/300?image=5',
+    numberLikes: 87,
+  },
+  {
+    id: i++,
+    autorName: 'Anthony Yamata',
+    autorImage: 'https://unsplash.it/300/300?image=7',
+    date: '02/04/2012',
+    postText: 'lorem ipsum dolor sit amet, consectet',
+    numberLikes: 42,
+  },
+  {
+    id: i++,
+    autorName: 'Negan Smith',
+    autorImage: 'https://unsplash.it/300/300?image=3',
+    date: '06/23/2015',
+    postText: 'lorem ipsum dolor sit amet, consectet',
+    postImage: 'https://unsplash.it/300/300?image=8',
+    numberLikes: 205,
+  },
+];
+
+console.log(post);
