6a991046c9e1d2fb8a599c67e57ad61f0ed172d6
Fernweh99 <daniele.dimento@gmail.com>
2022-07-06 15:33:22 +0200

    finish milestone 2

--- js/script.js
+++ js/script.js
@@ -1,36 +1,27 @@
 /*
-# Milestone 1
-Creiamo il nostro array di oggetti che rappresentano ciascun post.
-Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-- id del post, numero progressivo da 1 a n
-- nome autore,
-- foto autore,
-- data in formato americano (mm-gg-yyyy),
-- testo del post,
-- immagine (non tutti i post devono avere una immagine),
-- numero di likes.
-*Non è necessario creare date casuali, inventatele*
-*Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=3)
+#Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html,
+stampiamo i post del nostro feed.
 */
 
 let i = 1;
 
-const post = [
+const posts = [
   {
     id: i++,
     autorName: 'Negan Smith',
-    autorImage: 'https://unsplash.it/300/300?image=3',
+    autorImage: 'img/negan_smith.webp',
     date: '05/25/2015',
-    postText: 'lorem ipsum dolor sit amet, consectet',
+    postText: 'lorem ipsum dolor sit amet, consectet  amet, consectet amet, consectet',
     postImage: 'https://unsplash.it/300/300?image=8',
     numberLikes: 105,
   },
   {
     id: i++,
     autorName: 'Peter Parker',
-    autorImage: 'https://unsplash.it/300/300?image=4',
+    autorImage: 'img/peter_parker.jpeg',
     date: '10/21/2018',
-    postText: 'lorem ipsum dolor sit amet, consectet',
+    postText: 'lorem amet, consectet ipsum dolor amet, consectet sit amet, consectet',
     postImage: 'https://unsplash.it/300/300?image=5',
     numberLikes: 87,
   },
@@ -39,18 +30,60 @@ const post = [
     autorName: 'Anthony Yamata',
     autorImage: 'https://unsplash.it/300/300?image=7',
     date: '02/04/2012',
-    postText: 'lorem ipsum dolor sit amet, consectet',
+    postText: ' amet, consectet  amet, consectet lorem ipsum dolor sit amet, consectet',
     numberLikes: 42,
   },
   {
     id: i++,
     autorName: 'Negan Smith',
-    autorImage: 'https://unsplash.it/300/300?image=3',
+    autorImage: 'img/negan_smith.webp',
     date: '06/23/2015',
-    postText: 'lorem ipsum dolor sit amet, consectet',
+    postText: 'lorem ipsum amet, consectet dolor amet, consectet sit amet, consectet',
     postImage: 'https://unsplash.it/300/300?image=8',
     numberLikes: 205,
   },
 ];
 
-console.log(post);
+const postContainer = document.querySelector(".posts-list");
+
+let postList = "";
+for (const post of posts) {
+  postList += `<div class="post">
+  <div class="post__header">
+    <div class="post-meta">
+      <div class="post-meta__icon">
+        <img class="profile-pic" src="${post.autorImage}" alt="${post.autorName}" />
+      </div>
+      <div class="post-meta__data">
+        <div class="post-meta__author">${post.autorName}</div>
+        <div class="post-meta__time">${post.date}</div>
+      </div>
+    </div>
+  </div>
+  <div class="post__text">
+    ${post.postText}
+  </div>`
+
+  if (post.hasOwnProperty("postImage")) {
+  postList += `
+  <div class="post__image">
+    <img src="${post.postImage}" alt="" />
+  </div> `
+  }
+
+  postList += `
+  <div class="post__footer">
+    <div class="likes js-likes">
+      <div class="likes__cta">
+        <a class="like-button js-like-button" href="#" data-postid="${post.id}">
+          <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+          <span class="like-button__label">Mi Piace</span>
+        </a>
+      </div>
+      <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${post.numberLikes}</b> persone</div>
+    </div>
+  </div>
+</div>`
+}
+ 
+postContainer.innerHTML = postList;
