b7c2dd08c4cea721e6d21ff9fb6bbe91620ae402
Fernweh99 <daniele.dimento@gmail.com>
2022-07-06 14:39:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+# Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+- id del post, numero progressivo da 1 a n
+- nome autore,
+- foto autore,
+- data in formato americano (mm-gg-yyyy),
+- testo del post,
+- immagine (non tutti i post devono avere una immagine),
+- numero di likes.
+*Non è necessario creare date casuali, inventatele*
+*Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=3)
+*/
